2c1dfbe7126f0ac80d00e25820633f8f00976515
Vanja Kostic <vanjakostic03@gmail.com>
2025-12-21 16:44:49 +0100

    [ADD] - initial commit

---
+++ PKI-backend/src/main/java/com/ftn/pki/PkiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.pki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PkiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PkiBackendApplication.class, args);
+	}
+
+}
