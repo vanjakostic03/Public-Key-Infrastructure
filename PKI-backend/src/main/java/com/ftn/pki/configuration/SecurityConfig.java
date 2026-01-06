31b3f78df27013e197a7e0eb4aff6e43f1fa0378
Vanja Kostic <vanjakostic03@gmail.com>
2026-01-06 18:10:58 +0100

    [WIP] - token handling

--- PKI-backend/src/main/java/com/ftn/pki/configuration/SecurityConfig.java
+++ PKI-backend/src/main/java/com/ftn/pki/configuration/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.ftn.pki.security.RestAuthenticationEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,11 +13,11 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 @Configuration
@@ -32,16 +33,19 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/public/**").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt
                                 .jwtAuthenticationConverter(jwtAuthenticationConverter()))
-                )
-                .sessionManagement(session ->
+                ).exceptionHandling(ex ->
+                        ex.authenticationEntryPoint(restAuthenticationEntryPoint)
+                ).sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 );
 
@@ -71,5 +75,16 @@ public class SecurityConfig {
         return converter;
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("POST", "PUT", "GET", "OPTIONS", "DELETE", "PATCH")); // or simply "*"
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 }
--- PKI-backend/src/main/java/com/ftn/pki/controllers/authentication/AuthenticationController.java
+++ PKI-backend/src/main/java/com/ftn/pki/controllers/authentication/AuthenticationController.java
@@ -1,5 +1,39 @@
 package com.ftn.pki.controllers.authentication;
 
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthenticationController {
+    @PostMapping("/token")
+    public ResponseEntity<?> exchangeCode(@RequestBody Map<String, String> body) {
+        String code = body.get("code");
+
+        RestTemplate rest = new RestTemplate();
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", "pki-backend");
+        params.add("client_secret", "Vgyu0xweXpvG7D0jrggJZnR3RM09jzBC");
+        params.add("code", code);
+        params.add("redirect_uri", "http://localhost:4200");
+
+        ResponseEntity<Map> response =
+                rest.postForEntity(
+                        "http://localhost:8080/realms/pki/protocol/openid-connect/token",
+                        params,
+                        Map.class
+                );
+
+        return ResponseEntity.ok(response.getBody());
+    }
+
 }
