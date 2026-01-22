389e334f31276309ad202d98ea87183d13cbe1c3
Vanja Kostic <vanjakostic03@gmail.com>
2026-01-22 20:08:24 +0100

    [WIP] - certificates

--- PKI-backend/src/main/java/com/ftn/pki/entities/certificates/Certificate.java
+++ PKI-backend/src/main/java/com/ftn/pki/entities/certificates/Certificate.java
@@ -2,27 +2,46 @@ package com.ftn.pki.entities.certificates;
 
 import com.ftn.pki.entities.organizations.Organization;
 import com.ftn.pki.entities.users.User;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
+import java.util.UUID;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 
+@Entity
+//@Table("certificates")
 public class Certificate {
-    private Organization issuer;
-    private User subject;
-    private Date startDate;
-    private Date endDate;
-    private CertificateType certificateType;
-    private Boolean isRevoked;
-    private Boolean basicConstraints;
-    private Certificate parent;
+
+    @Id
+    private UUID id;
+
+    @ManyToOne
+    private CertificateAuthority issuerCa;
+
+    private String subjectDn;
+
+    @Lob
+    private String certificatePem;
+
+    private String serialNumber;
+
+    private Date validFrom;
+    private Date validTo;
+
+    @Enumerated(EnumType.STRING)
+    private CertificateType type; // ROOT, INTERMEDIATE, EE
+
+    private boolean revoked;
+    private Date revokedAt;
+    private String revocationReason;
 
 
 
--- PKI-backend/src/main/java/com/ftn/pki/repositories/certificates/CertificateRepository.java
+++ PKI-backend/src/main/java/com/ftn/pki/repositories/certificates/CertificateRepository.java
@@ -1,4 +1,11 @@
 package com.ftn.pki.repositories.certificates;
 
-public class CertificateRepository {
+import com.ftn.pki.entities.certificates.Certificate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CertificateRepository extends JpaRepository<Certificate, UUID> {
 }
