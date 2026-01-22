96b1ab1fda883e9d92af548b90dec84e768a6248
Vanja Kostic <vanjakostic03@gmail.com>
2026-01-22 15:34:27 +0100

    [WIP] - keycloak token

--- PKI-frontend/src/app/app.component.ts
+++ PKI-frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {ActivatedRoute, RouterOutlet} from '@angular/router';
 import {AuthService} from './authentication/auth.service';
+import {UserService} from './authentication/user.service';
 
 @Component({
   selector: 'app-root',
@@ -12,15 +13,23 @@ import {AuthService} from './authentication/auth.service';
 export class AppComponent {
   title = 'PKI-frontend';
 
-  constructor(private route: ActivatedRoute, private authService: AuthService) {}
+  constructor(private route: ActivatedRoute, private authService: AuthService, private userService: UserService) {}
 
   ngOnInit() {
+    console.log("onitin")
+
     this.route.queryParams.subscribe(params => {
-      const code = params['code'];
-      console.log("upao " + code);
-      if (code) {
-        this.authService.exchangeCode(code);
+      console.log('queryParams:', params); // direktno loguje objekat u konzolu
+      console.log('queryParams JSON:', JSON.stringify(params)); // pretvara u string za čitljiv prikaz
+
+      if (params['code']) {
+        console.log("if");
+        this.authService.exchangeCode(params['code']);
+      } else if (!this.userService.getToken()) {
+        console.log("elseif");
+        this.authService.login();
       }
     });
   }
+
 }
--- PKI-frontend/src/app/authentication/auth.service.ts
+++ PKI-frontend/src/app/authentication/auth.service.ts
@@ -9,13 +9,67 @@ export class AuthService {
 
   constructor(private http: HttpClient, private userService: UserService) {}
 
-  exchangeCode(code: string) {
-    this.http.post<any>('http://localhost:8081/api/auth/token', { code })
-      .subscribe(res => {
-        this.userService.storeToken(res.access_token);
-        window.history.replaceState({}, '', '/');
-      });
+  private clientId = 'pki-frontend';
+  private redirectUri = 'http://localhost:4200';
+
+  login() {
+    const verifier = this.generateCodeVerifier();
+    sessionStorage.setItem('pkce_verifier', verifier);
+
+    this.generateCodeChallenge(verifier).then(challenge => {
+      const authUrl = `http://localhost:8080/realms/pki/protocol/openid-connect/auth?` +
+        `client_id=${this.clientId}` +
+        `&response_type=code` +
+        `&redirect_uri=${encodeURIComponent(this.redirectUri)}` +
+        `&scope=openid` +
+        `&code_challenge=${challenge}` +
+        `&code_challenge_method=S256`;
+
+      console.log('Redirecting to Keycloak:', authUrl); // ovde vidiš tačan URL
+      window.location.href = authUrl;
+    });
   }
+
+
+  exchangeCode(code: string) {
+    const body = new URLSearchParams({
+      grant_type: 'authorization_code',
+      client_id: 'pki-frontend',
+      code: code,
+      redirect_uri: 'http://localhost:4200',
+      code_verifier: sessionStorage.getItem('pkce_verifier')!
+    });
+
+    this.http.post<any>(
+      'http://localhost:8080/realms/pki/protocol/openid-connect/token',
+      body.toString(),
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }
+    ).subscribe(res => {
+      this.userService.storeToken(res.access_token);
+      window.history.replaceState({}, '', '/');
+    });
+  }
+
+  private generateCodeVerifier(): string {
+      const array = new Uint8Array(32);
+      window.crypto.getRandomValues(array);
+      return Array.from(array, b => b.toString(16).padStart(2, '0')).join('');
+    }
+
+  private async generateCodeChallenge(verifier: string): Promise<string> {
+      const data = new TextEncoder().encode(verifier);
+      const digest = await crypto.subtle.digest('SHA-256', data);
+
+      return btoa(String.fromCharCode(...new Uint8Array(digest)))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+    }
+
 
 
 }
